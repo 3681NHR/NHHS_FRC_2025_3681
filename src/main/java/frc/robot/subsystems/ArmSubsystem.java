ad448864d0194298572958cca317cf08853435d8
potatinator <potatinator@proton.me>
2025-01-14 16:25:10 -0800

    e

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,12 +1,26 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.Constants;
 
 public class ArmSubsystem extends SubsystemBase {
     
+    private ProfiledPIDController anglePID = new ProfiledPIDController(Constants.arm.gains.ANGLE_KP,
+                                                                        Constants.arm.gains.ANGLE_KI,
+                                                                        Constants.arm.gains.ANGLE_KD,
+                                                                        null);
+    private ArmFeedforward angleFF = new ArmFeedforward(Constants.arm.gains.ANGLE_KS,
+                                                        Constants.arm.gains.ANGLE_KG,
+                                                        Constants.arm.gains.ANGLE_KV);
     public ArmSubsystem(){
 
+    }
+
+    @Override
+    public void periodic() {
+    
     }
 }
