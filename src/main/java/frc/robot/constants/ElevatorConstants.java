0e50d0ca5f02239ea663386a612f618faf64575f
i5 computer <i5@roboraiders.org>
2025-03-22 09:59:32 -0700

    Deploy at 'Sat Mar 22 09:59:31 PDT 2025'

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -39,7 +39,7 @@ public final class ElevatorConstants {
     public static final int CURRENT_LIM = 30;
     //5, 0, 2
     public static final PIDGains.ProfiledPID POS_PID = new PIDGains.ProfiledPID(10, 0, 3, 2.5, 5);
-    public static final PIDGains.GravityFF POS_FF = new PIDGains.GravityFF(0.25, 0.8, 5.2, 0.6);
+    public static final PIDGains.GravityFF POS_FF = new PIDGains.GravityFF(0.3, 0.75, 5.0, 0.6);
 
     public static final PIDGains.ProfiledPID POS_PID_SIM = new PIDGains.ProfiledPID(0, 0, 0, 1.75, 1.75*5);
     public static final PIDGains.GravityFF POS_FF_SIM = new PIDGains.GravityFF(0.19349, 0.35424, 4.5227, 0.3535);
